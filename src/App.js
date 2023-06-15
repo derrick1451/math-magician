081d4c3cb537d85da675f70024662da4509301ea
Derrick1451 <haylederrick14@gmail.com>
2023-06-15 13:45:02 +0300

    Update Ui of the application using routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Calcultor from './module/calculator';
 import Apps from './module/fetch';
+import Home from './module/Home';
+import NotMatch from './module/noMatch';
+import Layout from './module/Layout';
 
 function App() {
   return (
-    <div className="container">
-      <Calcultor />
-      <Apps />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="Calculator" element={<Calcultor />} />
+        <Route path="Quote" element={<Apps />} />
+        <Route path="*" element={<NotMatch />} />
+      </Route>
+    </Routes>
 
   );
 }
